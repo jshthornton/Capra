f2ce105890ab227e56319931820c1e06f107568c
Joshua Thornton <jshthornton@gmail.com>
2015-02-19 14:37:57 +0800

    Basic array list to minic some functionality of collections.
    This array list to allow for a non-intrusive method of storing a
    collection of data, which may or may not be a model, but will still
    allow for event driven programming, and also allow for a model to be
    bound to more than 1 array list. In contrast to collection in which a
    model can only belong to one, an array list will allow for a model to
    exist in unlimited number of arraylists, and even exist in the same
    array list. Ensuring no duplicates is a matter for the developer.

---
+++ src/ArrayList.js
@@ -0,0 +1,89 @@
+define([
+	'underscore',
+	'backbone'
+], function(_, Backbone) {
+	'use strict';
+
+	var Cls = function() {
+		this.initialize.apply(this, arguments);
+	};
+
+	_.extend(Cls.prototype, Backbone.Events, {
+		// items: []
+		length: 0,
+
+		initialize: function() {
+			_.bindAll(this);
+
+			this.items = [];
+
+			this.on('add', this.onAdd);
+			this.on('remove', this.onRemove);
+			this.on('clear', this.onClear);
+		},
+
+		_add: function(item, index) {
+			if(_.isNumber(index)) {
+				this.items.splice(index, 0, item);
+			} else {
+				this.items.push(item);
+			}
+
+			this.trigger('add', this, item, this.items);
+		},
+
+		add: function(data, options) {
+			options = _.assign({
+				multiple: false
+			}, options);
+
+			if(options.multiple === true) {
+				// Not using concat here due to event firing timing.
+				var at = options.at;
+				_.each(data, function(item) {
+					this._add(item, at);
+
+					// This is to stop the next item from being added before the previous.
+					if(_.isNumber(at)) {
+						at++;
+					}
+				}, this);
+			} else {
+				this._add(data, options.at);
+			}
+		},
+
+		remove: function(index) {
+			var removed = this.items.splice(index, 1);
+			this.trigger('remove', this, removed, this.items);
+		},
+
+		at: function(index) {
+			return this.items[index];
+		},
+
+		clear: function() {
+			this.items.length = 0;
+			this.trigger('clear', this);
+		},
+
+		_updateLength: function() {
+			this.length = this.items.length;
+		},
+
+		// Internal Events
+		onAdd: function() {
+			this._updateLength();
+		},
+
+		onRemove: function() {
+			this._updateLength();
+		},
+
+		onClear: function() {
+			this._updateLength();
+		}
+	});
+
+	return Cls;
+});
---
+++ test/ArrayListSpec.js
@@ -0,0 +1,74 @@
+define(['capra/ArrayList', 'backbone'], function(ArrayList, Backbone) {
+	beforeEach(function() {
+		this.evt = _.extend({}, Backbone.Events);
+		this.arrayList = new ArrayList();
+	});
+
+	it('Should create an instance', function() {
+		expect(this.arrayList instanceof ArrayList).toEqual(true);
+	});
+
+	it('Should update length (add)', function() {
+		expect(this.arrayList.length).toEqual(0);
+
+		this.arrayList.add(true);
+		expect(this.arrayList.length).toEqual(1);
+	});
+
+	it('Should update length (remove)', function() {
+		expect(this.arrayList.length).toEqual(0);
+
+		this.arrayList.add(true);
+		expect(this.arrayList.length).toEqual(1);
+
+		this.arrayList.remove(0);
+		expect(this.arrayList.length).toEqual(0);
+	});
+
+	it('Should update length (clear)', function() {
+		expect(this.arrayList.length).toEqual(0);
+		
+		this.arrayList.add(true);
+		expect(this.arrayList.length).toEqual(1);
+
+		this.arrayList.clear();
+		expect(this.arrayList.length).toEqual(0);
+	});
+
+	it('Should be able to get element', function() {
+		expect(this.arrayList.length).toEqual(0);
+		
+		this.arrayList.add(true);
+		expect(this.arrayList.at(0)).toEqual(true);
+	});
+
+	it('Should be able to add multiple', function() {
+		expect(this.arrayList.length).toEqual(0);
+		
+		this.arrayList.add([0, 1, 2], {
+			multiple: true
+		});
+		expect(this.arrayList.at(0)).toEqual(0);
+		expect(this.arrayList.at(1)).toEqual(1);
+		expect(this.arrayList.at(2)).toEqual(2);
+	});
+
+	it('Should be able to add multiple (at index)', function() {
+		expect(this.arrayList.length).toEqual(0);
+		
+		this.arrayList.add(true);
+
+		this.arrayList.add([0, 1, 2], {
+			multiple: true,
+			at: 0
+		});
+
+		expect(this.arrayList.at(0)).toEqual(0);
+		expect(this.arrayList.at(1)).toEqual(1);
+		expect(this.arrayList.at(2)).toEqual(2);
+	});
+
+	afterEach(function() {
+		this.evt.stopListening();
+	});
+});
