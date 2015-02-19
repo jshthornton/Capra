627db47ef8bb4d2923922d08b1a573ef6f3d5499
Joshua Thornton <jshthornton@gmail.com>
2015-02-19 21:35:26 +0800

    Stateful object.
    This is similar to Backbone.Model, except it offers overridable getters
    and setters. It also does not have all of the bloat that model does.

---
+++ src/Stateful.js
@@ -0,0 +1,82 @@
+define([
+	'underscore',
+	'backbone'
+], function(_, Backbone) {
+	'use strict';
+
+	var Cls = function() {
+		this.initialize.apply(this, arguments);
+	};
+
+	_.extend(Cls.prototype, Backbone.Events, {
+		// properties
+
+		initialize: function() {
+			_.bindAll(this);
+
+			this._previous = {};
+			this._properties = {};
+		},
+
+		set: function() {
+			var props,
+				options;
+
+			if(_.isObject(arguments[0])) {
+				props = arguments[0];
+				options = arguments[1];
+			} else {
+				props = {};
+				props[arguments[0]] = arguments[1];
+				options = arguments[2];
+			}
+
+			this._fireChange = false;
+
+			_.forOwn(props, function(value, key) {
+				var fnString = '_' + key + 'Setter';
+
+				if(_.isFunction(this[fnString])) {
+					this[fnString](key, value, options);
+				} else {
+					this._set(key, value, options);
+				}
+			}, this);
+
+			if(this._fireChange === true) {
+				this.trigger('change', this, options);
+			}
+		},
+
+		get: function(key, options) {
+			var fnString = '_' + key + 'Getter';
+
+			if(_.isFunction(this[fnString])) {
+				return this[fnString](key, options);
+			} else {
+				return this._get(key);
+			}
+		},
+
+		previous: function(key) {
+			return this._previous[key];
+		},
+
+		_set: function(key, value, options) {
+			// Replace previous
+			this._previous[key] = this._properties[key];
+
+			// Update property
+			this._properties[key] = value;
+
+			this.trigger('change:' + key, this, value, options);
+			this._fireChange = true;
+		},
+
+		_get: function(key) {
+			return this._properties[key];
+		}
+	});
+
+	return Cls;
+});
---
+++ test/StatefulSpec.js
@@ -0,0 +1,61 @@
+define([
+	'capra/Stateful',
+	'backbone'
+], function(Stateful, Backbone) {
+	describe('capra/Stateful', function() {
+		beforeEach(function() {
+			this.evt = _.extend({}, Backbone.Events);
+			this.stateful = new Stateful();
+		});
+
+		it('Should create an instance', function() {
+			expect(this.stateful instanceof Stateful).toEqual(true);
+		});
+
+		it('Should set / get', function() {
+			this.stateful.set('test', true);
+
+			expect(this.stateful.get('test')).toEqual(true);
+		});
+
+		it('Should set previous', function() {
+			this.stateful.set('test', true);
+			this.stateful.set('test', false);
+
+			expect(this.stateful.get('test')).toEqual(false);
+			expect(this.stateful.previous('test')).toEqual(true);
+		});
+
+		it('Should support hash set', function() {
+			this.stateful.set({
+				test: true
+			});
+
+			expect(this.stateful.get('test')).toEqual(true);
+		});
+
+		it('Should set / get (custom)', function() {
+			this.stateful._testSetter = function(key, value, options) {
+				this._set(key, 'goat', options);
+			};
+
+			this.stateful._testGetter = function(key, options) {
+				return this._get(key, options);
+			};
+
+			this.stateful.set('test', true);
+
+			expect(this.stateful.get('test')).toEqual('goat');
+		});
+
+		it('Should not fire change (empty)', function() {
+			this.stateful.set({});
+
+			expect(this.stateful._fireChange).toEqual(false);
+		});
+
+		afterEach(function() {
+			this.evt.stopListening();
+		});
+	});
+});
