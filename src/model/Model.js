922f4e99905b5b4d759be5aa36110275ff1da708
Joshua Thornton <jshthornton@gmail.com>
2015-02-23 22:55:19 +0800

    Base for the models.

---
+++ src/model/Model.js
@@ -0,0 +1,41 @@
+define([
+	'ring',
+	'backbone',
+	'underscore'
+], function(ring, Backbone, _) {
+	return ring.create([Backbone.Model], {
+		cidPrefix: 'model',
+
+		constructor: function(attributes, options) {
+			var attrs = attributes || {};
+			options = options || {};
+
+			this.cid = _.uniqueId(this.cidPrefix);
+			this.attributes = {};
+
+			//this.validator = new Rulebot();
+			//this.initValidation();
+
+			//this.initEvents();
+
+			if (options.collection) {
+				this.collection = options.collection;
+			}
+			if (options.parse) {
+				attrs = this.parse(attrs, options) || {};
+			}
+
+			attrs = _.defaults({}, attrs, _.result(this, 'defaults'));
+			this.set(attrs, options);
+			this.changed = {};
+
+			this.initialize.apply(this, arguments);
+		},
+
+		initialize: function() {
+			_.bindAll(this);
+			this._super.apply(this, arguments);
+		},
+
+	});
+});
---
+++ test/model/ModelSpec.js
@@ -0,0 +1,5 @@
+define([
+	'capra/model/Model'
+], function(Model) {
+
+});
