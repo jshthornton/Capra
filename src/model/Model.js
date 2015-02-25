8d359a94d6f94a7b032e12819a73f81eb9929eb2
Joshua Thornton <joshua.thornton@readingroom.com>
2015-02-25 17:11:45 +0800

    Removed current fluff.

--- src/model/Model.js
+++ src/model/Model.js
@@ -4,37 +4,8 @@ define([
 	'underscore'
 ], function(ring, Backbone, _) {
 	return ring.create([Backbone.Model], {
-		cidPrefix: 'model',
-
-		constructor: function(attributes, options) {
-			var attrs = attributes || {};
-			options = options || {};
-
-			this.cid = _.uniqueId(this.cidPrefix);
-			this.attributes = {};
-
-			//this.validator = new Rulebot();
-			//this.initValidation();
-
-			//this.initEvents();
-
-			if (options.collection) {
-				this.collection = options.collection;
-			}
-			if (options.parse) {
-				attrs = this.parse(attrs, options) || {};
-			}
-
-			attrs = _.defaults({}, attrs, _.result(this, 'defaults'));
-			this.set(attrs, options);
-			this.changed = {};
-
-			this.initialize.apply(this, arguments);
-		},
-
 		initialize: function() {
 			_.bindAll(this);
-		},
-
+		}
 	});
 });
