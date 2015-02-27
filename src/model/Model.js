affcfbf3a8891fd78f64347fa1576b9995e7bafc
Joshua Thornton <joshua.thornton@readingroom.com>
2015-02-27 17:12:11 +0800

    Broken WIP pass off for model work.

--- src/model/Model.js
+++ src/model/Model.js
@@ -1,11 +1,98 @@
 define([
 	'ring',
 	'backbone',
-	'underscore'
-], function(ring, Backbone, _) {
+	'underscore',
+	'../cache/AbstractCache',
+	'jquery'
+], function(ring, Backbone, _, AbstractCache, $) {
 	return ring.create([Backbone.Model], {
+		expiry: 60,
+
 		initialize: function() {
 			_.bindAll(this);
+		},
+
+		_cacheRequest: function(options) {
+			options = _.defaults({
+				expiry: this.expiry
+			}, options);
+
+			if(_.has(options, 'data') === false) {
+				throw new Error('No data to cache');
+			}
+
+			if(ring.instance(options.cache, AbstractCache) === false) {
+				throw new Error('Cache not provided');
+			}
+
+			options.cache.set(options.url, options.data, {
+				expiry: options.expiry
+			});
+		},
+
+		_cachedSync: function(method, model, options) {
+			var syncFn = Backbone.Model.prototype.sync,
+				store, 
+				cache;
+
+			try {
+				store = this.collection.store;
+				cache = store.requestCache;
+			} catch(err) {}
+
+			if(cache != null) {
+				if(cache.exists(options.url)) {
+					rtn = cache.get(options.url);
+					var def = new $.Deferred();
+
+					
+
+					return def.promise();
+				} else {
+					rtn = syncFn.call(this, method, model, options);
+					
+					if(options.setCache === true) {
+						this._cacheRequest({
+							data: rtn,
+							cache: cache,
+							url: options.url
+						});
+					}
+				}
+			} else {
+				rtn = syncFn.call(this, method, model, options);
+			}
+
+			return rtn;
+		},
+
+		_uncachedSync: function(method, model, options) {
+			return Backbone.Model.sync.call(this, method, model, options);
+		},
+
+		sync: function(method, model, options) {
+			var store,
+				cache,
+				rtn;
+
+			options = _.defaults({
+				url: _.result(this, 'url')
+			}, options);
+
+			if(method === 'read') {
+				options = _.defaults(options, {
+					setCache: true,
+					useCache: true
+				});
+
+				if(options.useCache === true) {
+					return this._cachedSync(method, model, options);
+				} else {
+					return this._uncachedSync(method, model, options);
+				}
+			} else {
+				return this._uncachedSync(method, model, options);
+			}
 		}
 	});
 });
--- test/model/ModelSpec.js
+++ test/model/ModelSpec.js
@@ -1,5 +1,91 @@
 define([
-	'capra/model/Model'
-], function(Model) {
+	'capra/model/Model',
+	'capra/Store',
+	'capra/collection/Collection',
+	'underscore'
+], function(Model, Store, Collection, _) {
+	describe('capra/model/Model', function() {
+		beforeEach(function() {
+			jasmine.Ajax.install();
 
+			this.store = new Store();
+			this.collection = new Collection();
+			this.collection.url = '/collection';
+			this.model = new Model();
+
+			this.store.register('collection', this.collection);
+			this.collection.add(this.model);
+		});
+
+		afterEach(function() {
+			jasmine.Ajax.uninstall();
+		});
+
+		it('Should not store request in cache', function() {
+			this.model.set('id', 1);
+
+			expect(this.store.requestCache.length).toEqual(0);
+
+			this.model.fetch({
+				setCache: false
+			});
+
+			expect(this.store.requestCache.length).toEqual(0);
+		});
+
+		it('Should store request in cache', function() {
+			this.model.set('id', 1);
+
+			expect(this.store.requestCache.length).toEqual(0);
+
+			this.model.fetch();
+
+			expect(this.store.requestCache.length).toEqual(1);
+		});
+
+		it('Should only make 1 request', function() {
+			this.model.set('id', 1);
+
+			var rtn1 = this.model.fetch();
+			var rtn2 = this.model.fetch();
+
+			expect(jasmine.Ajax.requests.count()).toEqual(1);
+
+		});
+
+		it('Should return cached request', function() {
+			this.model.set('id', 1);
+
+			var rtn1 = this.model.fetch();
+			var rtn2 = this.model.fetch();
+
+			expect(rtn1).toBeTruthy();
+			expect(rtn1).not.toEqual(rtn2);
+
+		});
+
+		it('Should call success for cached', function() {
+			this.model.set('id', 1);
+
+			var onSuccess1 = jasmine.createSpy('onSuccess1'),
+				onSuccess2 = jasmine.createSpy('onSuccess2'); 
+
+			var rtn1 = this.model.fetch({
+				success: onSuccess1
+			});
+			var rtn2 = this.model.fetch({
+				success: onSuccess2
+			});
+
+			jasmine.Ajax.requests.mostRecent().respondWith({
+				'status': 200,
+				'contentType': 'application/json',
+				'responseText': '{}'
+			});
+
+			expect(onSuccess1).toHaveBeenCalled();
+			expect(onSuccess2).toHaveBeenCalled();
+
+		});
+	});
 });
