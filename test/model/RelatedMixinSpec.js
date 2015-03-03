6db706088abfac50f73f8fdae14a1d30cd84b741
Joshua Thornton <joshua.thornton@readingroom.com>
2015-03-03 15:00:09 +0800

    Added tests for nested tree searching. Added better structure to the tests to ensure that relationships are persistent to the model. This will eliminate new model instances (not spawned with relationships) from producing errors.

--- test/model/RelatedMixinSpec.js
+++ test/model/RelatedMixinSpec.js
@@ -17,42 +17,69 @@ define([
 		});
 
 		describe('fetchTree', function() {
-			var user, profile, users, profiles;
+			var user, profile, gender, users, profiles, genders;
 
 			beforeEach(function() {
 				jasmine.Ajax.install();
 
-				user = new FauxModel();
-				profile = new FauxModel();
+				var User = ring.create([FauxModel], {
+					relationships: [
+						{
+							key: 'profile',
+							type: 'hasOne',
+							collection: 'profiles',
+							foreignKey: 'user'
+						}
+					]
+				});
+
+				var Profile = ring.create([FauxModel], {
+					relationships: [
+						{
+							key: 'user',
+							type: 'hasOne',
+							collection: 'users',
+							foreignKey: 'profile'
+						},
+						{
+							key: 'gender',
+							type: 'hasOne',
+							collection: 'genders',
+							foreignKey: 'profiles'
+						}
+					]
+				});
+
+				var Gender = ring.create([FauxModel], {
+					relationships: [
+						{
+							key: 'profiles',
+							type: 'hasMany',
+							collection: 'profiles',
+							foreignKey: 'gender'
+						}
+					]
+				});
+
+				user = new User();
+				profile = new Profile();
+				gender = new Gender();
+
 				users = new Collection();
 				profiles = new Collection();
+				genders = new Collection();
 
-				users.model = FauxModel;
-				profiles.model = FauxModel;
+				users.model = User;
+				profiles.model = Profile;
+				genders.model = Gender;
 
 				users.url = '/users';
 				profiles.url = '/profiles';
-
-				user.relationships = [
-					{
-						key: 'profile',
-						type: 'hasOne',
-						collection: 'profiles',
-						foreignKey: 'user'
-					}
-				];
-
-				profile.relationships = [
-					{
-						key: 'user',
-						type: 'hasOne',
-						collection: 'users',
-						foreignKey: 'profile'
-					}
-				];
+				genders.url = '/genders';
 
 				this.store.register('users', users);
 				this.store.register('profiles', profiles);
+				this.store.register('genders', genders);
 				
 				users.add(user);
 				profiles.add(profile);
@@ -137,6 +164,40 @@ define([
 				expect(_profile.id).toEqual(1);
 			});
 
+			it('Should return a promise (branch) (hasOne -> hasMany)', function() {
+				jasmine.Ajax.stubRequest('/profiles/1').andReturn({
+					'status': 200,
+					'contentType': 'application/json',
+					'responseText': '{ "id": 1, "gender": 1 }'
+				});
+
+				jasmine.Ajax.stubRequest('/genders/1').andReturn({
+					'status': 200,
+					'contentType': 'application/json',
+					'responseText': '{ "id": 1 }'
+				});
+
+				profile.set('id', 1);
+
+				var promise = profile.fetchTree({
+					contains: {
+						gender: null
+					}
+				});
+
+				expect(jasmine.Ajax.requests.count()).toEqual(2);
+				expect(jasmine.Ajax.requests.first().url).toEqual('/profiles/1');
+				expect(jasmine.Ajax.requests.mostRecent().url).toEqual('/genders/1');
+
+				expect(promise).toBeTruthy();
+				expect(promise.state).toBeTruthy();
+
+				var _gender = this.store.getRelated(profile, 'gender');
+				expect(_gender).toBeTruthy();
+				expect(_gender.id).toEqual(1);
+				expect(store.getRelated(_gender, 'profiles').length).toEqual(1);
+			});
+
 			it('Should resolve after response (leaf)', function() {
 				var promise = user.fetchTree();
 
@@ -183,6 +244,43 @@ define([
 				});
 
 				expect(promise.state()).toEqual('resolved');
+			});
+
+			it('Should support nested branching', function() {
+				jasmine.Ajax.stubRequest('/users/1').andReturn({
+					'status': 200,
+					'contentType': 'application/json',
+					'responseText': '{ "id": 1, "profile": 1 }'
+				});
+
+				jasmine.Ajax.stubRequest('/profiles/1').andReturn({
+					'status': 200,
+					'contentType': 'application/json',
+					'responseText': '{ "id": 1, "gender": 1 }'
+				});
+
+				jasmine.Ajax.stubRequest('/genders/1').andReturn({
+					'status': 200,
+					'contentType': 'application/json',
+					'responseText': '{ "id": 1 }'
+				});
+
+				user.set('id', 1);
+
+				var promise = user.fetchTree({
+					contains: {
+						profile: {
+							contains: {
+								gender: null
+							}
+						}
+					}
+				});
+				
+				expect(jasmine.Ajax.requests.count()).toEqual(3);
+				expect(jasmine.Ajax.requests.first().url).toEqual('/users/1');
+				expect(jasmine.Ajax.requests.at(1).url).toEqual('/profiles/1');
+				expect(jasmine.Ajax.requests.mostRecent().url).toEqual('/genders/1');
 			});
 		});
 
