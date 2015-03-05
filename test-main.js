7d952773284912a99a71ad74809a54d932f495b2
Joshua Thornton <joshua.thornton@readingroom.com>
2015-03-05 11:16:03 +0800

    Keys fix for lower end browsers.

--- test-main.js
+++ test-main.js
@@ -5,12 +5,12 @@ var pathToModule = function(path) {
   return path.replace(/^\/base\//, '').replace(/\.js$/, '');
 };
 
-Object.keys(window.__karma__.files).forEach(function(file) {
+for (var file in window.__karma__.files) {
   if (TEST_REGEXP.test(file)) {
     // Normalize paths to RequireJS module names.
     allTestFiles.push(pathToModule(file));
   }
-});
+}
 
 require.config({
   // Karma serves files under /base, which is the basePath from your config file
