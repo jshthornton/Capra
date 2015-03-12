3d71015d6a81666386e8ac5a64d6df5105ac8cfc
Joshua Thornton <jshthornton@gmail.com>
2015-03-12 21:26:00 +0800

    Spec for collection.

---
+++ test/collection/CollectionSpec.js
@@ -0,0 +1,39 @@
+define([
+	'ring',
+	'underscore',
+	'capra/collection/Collection',
+	'capra/model/Model'
+], function(ring, _, Collection, Model) {
+	describe('capra/collection/Collection', function() {
+		it('Should keep the same model', function() {
+			/*var FauxCollection = ring.create([Collection], {
+				constructor: function() {
+					console.log('here');
+					this.model = Model;
+					this.$super.apply(this, arguments);
+				}
+			});
+
+			var collection = new FauxCollection();
+			expect(collection.model).toEqual(Model);*/
+
+			var A = ring.create({
+
+			});
+
+			var C = {};
+
+			var B = ring.create({
+				constructor: function() {
+					_.bindAll(this);
+
+					this.item = A;
+				}
+			});
+
+			var b = new B();
+
+			expect(b.item).toEqual(A);
+		});
+	});
+});
