577e547d0daff7f2cd032e02d79de63391a877f1
Joshua Thornton <joshua.thornton@readingroom.com>
2015-03-17 15:55:46 +0800

    Opening tag util for generating opening tags of HTML. Not complete. Not tested.

--- src/util/element.js
+++ src/util/element.js
@@ -14,6 +14,49 @@ define([
 			}
 
 			return $.contains(document.documentElement, element);
+		},
+
+		openTag: function(options) {
+			function toAttr(key, value) {
+				var parsedKey,
+					parsedValue;
+
+				switch(key) {
+					case 'tagName':
+						return value;
+					case 'className':
+						parsedKey = 'class';
+
+						if(_.isArray(value)) {
+							parsedValue = value.join(' ');
+						}
+						break;
+					default:
+						parsedKey = key;
+						parsedValue = value;
+						break;
+				}
+
+				if(parsedKey !== null && parsedValue !== null) {
+					return parsedKey + '=' + '"' + parsedValue + '"';
+				} else {
+					return undefined;
+				}
+			};
+
+			options = _.extend({
+				tagName: 'div'
+			}. options);
+
+			var el = ['<'];
+			
+			_.forOwn(options, function(value, key) {
+				el.push(toAttr(key, value));
+			});
+
+			el.push('>');
+
+			return el.join(' ');
 		}
 	};
 });
